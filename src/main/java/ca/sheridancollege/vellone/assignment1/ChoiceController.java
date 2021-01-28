bf09bd2ec2201f1bbe3acb9edd7eb51350f75219
vellone20 <vellone@sheridancollege.ca>
2021-01-27 19:14:20 -0500

    Trying different solutions to implement the math needed

--- src/main/java/ca/sheridancollege/vellone/assignment1/ChoiceController.java
+++ src/main/java/ca/sheridancollege/vellone/assignment1/ChoiceController.java
@@ -6,7 +6,8 @@ package ca.sheridancollege.vellone.assignment1;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Random;
@@ -20,31 +21,46 @@ public class ChoiceController {
         return "index";
     }
 
-    @PostMapping("/game")
-    public String processChoice(Model model, @RequestParam int value){
-
-        Random rn = new Random();
-        int cAnswer = 0;
-        for(int i=0; i < 1; i++)
-        {
-            int answer = rn.nextInt(3) + 1;
-            cAnswer = answer;
-        }
-
-        if (cAnswer > value)
-        {
-            model.addAttribute("winner", "you win");
-            return "output";
-        }
-        else if (cAnswer < value)
-        {
-            model.addAttribute("winner", "you lose");
-            return "output";
-        }
-        else
-        {
-            model.addAttribute("winner", "tie");
-            return "output";
-        }
+    @RequestMapping("/game")
+    public String choice1(Model model, @RequestParam String selection1) {
+        model.addAttribute("name", selection1);
+        return "output";
+    }
+    public String choice2(Model model, @RequestParam String selection2) {
+        model.addAttribute("name", selection2);
+        return "output";
+    }
+    public String choice3(Model model, @RequestParam String selection3) {
+        model.addAttribute("name", selection3);
+        return "output";
     }
 }
+
+
+//        Random rn = new Random();
+//        int cAnswer = 0;
+//        for(int i=0; i < 1; i++)
+//        {
+//            int answer = rn.nextInt(3) + 1;
+//            cAnswer = answer;
+//        }
+
+
+
+
+
+//        if (cAnswer > value)
+//        {
+//            model.addAttribute("result", "Winner");
+//            return "output";
+//        }
+//        else if (cAnswer < value)
+//        {
+//            model.addAttribute("winner", "you lose");
+//            return "output";
+//        }
+//        else
+//        {
+//            model.addAttribute("winner", "tie");
+//            return "output";
+//        }
